59ade7748b64cef713f2e17627f532c4f5b99512
Ron Vincent <rovin@microsoft.com>
2020-03-26 15:41:57 -0400

    material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import AzureMap from "./components/AzureMap";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
       <AzureMap />
     </div>
   );
